b16abed428d7513850077bd32bab2b571274de94
Aidan HM <aidanhm@MacBook-Pro-6.local>
2026-05-22 03:47:28 +0200

    landing: 'still building' back in hero; founder intro → 'Coming in June.'

--- app/sw.js
+++ app/sw.js
@@ -6,7 +6,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v612';
+const CACHE_NAME = 'debateos-v613';
 
 
 
--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v612';
+const CACHE_NAME = 'debateos-v613';
 
 
 
