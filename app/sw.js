3cfd8df5556d4b17cd201f4f9cfd625d025de4e8
Aidan HM <aidanhm@MacBook-Pro-6.local>
2026-06-15 09:34:35 -0400

    Revert "landing(live-debates): rebuild the band as an asymmetric live room"
    This reverts commit 5e2a1a31181da3d58b46dc21728e8249b1af0bc0.

--- app/sw.js
+++ app/sw.js
@@ -6,7 +6,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1004';
+const CACHE_NAME = 'debateos-v1005';
 
 
 
--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1004';
+const CACHE_NAME = 'debateos-v1005';
 
 
 
