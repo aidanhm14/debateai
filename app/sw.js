df869d5e1719582dbc9a511a3b186f95eb3da01b
Aidan HM <aidanhm@MacBook-Pro-6.local>
2026-06-15 13:44:53 -0400

    landing(live): more space between the 'Debate real people' title and the format chips

--- app/sw.js
+++ app/sw.js
@@ -6,7 +6,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1076';
+const CACHE_NAME = 'debateos-v1077';
 
 
 
--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1076';
+const CACHE_NAME = 'debateos-v1077';
 
 
 
