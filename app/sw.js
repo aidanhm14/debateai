07bc08b9abe63a41469e1f2f72e81b065133457d
Aidan HM <aidanhm@MacBook-Pro-6.local>
2026-06-25 10:52:18 -0400

    Expand Atlas contact coverage

--- app/sw.js
+++ app/sw.js
@@ -6,7 +6,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1250';
+const CACHE_NAME = 'debateos-v1251';
 
 
 
--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1250';
+const CACHE_NAME = 'debateos-v1251';
 
 
 
