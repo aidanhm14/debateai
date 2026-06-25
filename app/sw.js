eb3a3eb2a8f678d6f94df384df972415be0c1427
Aidan HM <aidanhm@MacBook-Pro-6.local>
2026-06-25 11:07:10 -0400

    atlas: add 80 WSDC national debate-org pins (global circuit, contacts in Firestore)

--- app/sw.js
+++ app/sw.js
@@ -6,7 +6,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1254';
+const CACHE_NAME = 'debateos-v1255';
 
 
 
--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1254';
+const CACHE_NAME = 'debateos-v1255';
 
 
 
