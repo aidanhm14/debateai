84ce07ff863e895cdd1350fec61385f97fcf18e3
Aidan HM <aidanhm@MacBook-Pro-6.local>
2026-06-25 10:24:22 -0400

    Add Tabroom national-circuit Atlas programs

--- app/sw.js
+++ app/sw.js
@@ -6,7 +6,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1243';
+const CACHE_NAME = 'debateos-v1244';
 
 
 
--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1243';
+const CACHE_NAME = 'debateos-v1244';
 
 
 
