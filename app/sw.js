7a2fe8e9a8add55ab4934aa04d9e853a7b93fab5
Aidan HM <aidanhm@MacBook-Pro-6.local>
2026-06-27 15:40:20 -0500

    landing: remove the rotating creed line from the hero (both arms)

--- app/sw.js
+++ app/sw.js
@@ -6,7 +6,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1280';
+const CACHE_NAME = 'debateos-v1281';
 
 
 
--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1280';
+const CACHE_NAME = 'debateos-v1281';
 
 
 
