65d54696b490c00edc17639fa549eb88da7524e1
Aidan HM <aidanhm@MacBook-Pro-6.local>
2026-07-02 02:31:27 -0400

    Ensure trained debate video plays

--- app/sw.js
+++ app/sw.js
@@ -6,7 +6,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1361';
+const CACHE_NAME = 'debateos-v1362';
 
 
 
--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 
 
-const CACHE_NAME = 'debateos-v1361';
+const CACHE_NAME = 'debateos-v1362';
 
 
 
